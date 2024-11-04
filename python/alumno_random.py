3b110f4cbf4057289af7ebc87996ea834e427100
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-11-04 08:42:12 +0100

    Create alumno_random.py

---
+++ python/alumno_random.py
@@ -0,0 +1,9 @@
+import random
+alumnos = ["Beatriz", "Sara", "Jimena", "Helena", "Romualdo", "Silvia", "Lorena", "Ángel",
+            "Samuel", "Alejandro", "Marina H.", "Artiom", "Paula", "Elena", "Marina M.",
+           "Victor", "Juan David", "Mar", "María", "Iván", "Marta", "David",
+           "Leonardo", "Alejandra"]
+print("Total alumnos: ", len(alumnos))
+
+print(random.choice(alumnos))
+
